d7c842f52ede17b239577aad9dd104775db907e3
olexivas <olexivas@gmail.com>
2020-01-14 16:12:23 +0200

    Project's finished

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 
-export class App extends Component {
-  state = {
-    data: []
-  };
+import "./App.scss";
+
+class App extends Component {
+  state = { data: [] };
 
   fetchApi = () => {
-      fetch("https://api.quotable.io/random")
+    fetch("https://api.quotable.io/random")
       .then(res => res.json())
-      .then(data =>
-        this.setState({
-          data
-        })
-      );
+      .then(data => this.setState({ data }));
   };
 
   handleClick = () => {
     this.fetchApi();
-  }
+  };
 
   componentDidMount = () => {
-      this.fetchApi();
-  }
+    this.fetchApi();
+  };
+
 
   render() {
     const { content, author } = this.state.data;
+    const quote = `https://twitter.com/intent/tweet?text=${content}`;
 
     return (
       <div className="wrapper">
-        <div className="quote-box">
-          <p className="quote-text">
-            {content}
-          </p>
-          <p className="quote-author">{author}</p>
-        </div>
-
-        <div className="buttons">
-          <a href="#" className="btn">
-            Twitter
-          </a>
-          <a href="#" className="btn" onClick={this.handleClick}>
-            New Quote
-          </a>
+        <div className="quote-box" id="quote-box">
+          <p className="quote-text" id="text">{content}</p>
+          <p className="quote-author" id="author">{author}</p>
+          <div className="buttons">
+            <a id="tweet-quote"href={quote} className="btn" target="_blank">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a
+              href="#"
+              className="btn"
+              id="new-quote"
+              onClick={this.handleClick}
+            >
+              New quote
+            </a>
+          </div>
         </div>
 
         <footer className="footer">
